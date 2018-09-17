5e0d37c456f04026e2adaf54430167fc56e2a901
DaviSM <davimesquita@gmail.com>
2018-09-16 21:43:11 -0300

    iroha certificates

---
+++ docker/iroha-ca/iroha-ca.sh
@@ -0,0 +1,23 @@
+if [ -z "$1" ]
+then
+    echo ""
+    echo "#############"
+    echo "# Usage: account password"
+    echo "#"
+    echo "# Example: docker run --rm frkr/iroha-ca alice@ru mysupersecretpassword"
+    echo "#############"
+else
+    iroha-cli --new_account --account_name $1 --pass_phrase $2 --key_path ./
+    echo ""
+    echo "Account:" $1
+    echo "PRIV ############# PRIV ############# PRIV"
+    cat $1.priv
+    echo ""
+    rm $1.priv
+    echo "PRIV ############# PRIV ############# PRIV"
+    echo "PUB ############# PUB ############# PUB"
+    cat $1.pub
+    echo ""
+    rm $1.pub
+    echo "PUB ############# PUB ############# PUB"
+fi
---
+++ docker/iroha-ca/push.bat
@@ -0,0 +1,1 @@
+docker push frkr/iroha-ca
